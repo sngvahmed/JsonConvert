ffd6eb22b661d3b193a8b4e216c7370837d77bb2
Ahmed Nasser <ahmednasser1993@gmail.com>
2019-09-01 18:39:22 +0200

    init the fiolter

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const { clipboard, remote } = require('electron')
 const fs = require('fs');
 const { Parser } = require('json2csv');
+const jsonTemplate = require("./template");
 const dialog = remote.dialog;
 WIN = remote.getCurrentWindow();
 
-var value;
+const keyFilters = [];
+let counter = 0;
+
 $('#paste').click(() => {
     try {
         const t = clipboard.readText('clipboard');
         value = JSON.parse(t);
+        renderKeys(value);
         $('#json-renderer').jsonViewer(value);
     } catch (err) {
         console.log("Not Valid Json");
     }
 });
 
+setTimeout(() => {
+    $('#json-renderer').jsonViewer(jsonTemplate);
+    renderKeys(jsonTemplate);
+    renderKeysOption();
+}, 300)
 
 $('#saveCsv').click(() => {
     
@@ -57,3 +66,45 @@ $('#saveCsv').click(() => {
         console.error(err);
     }
 });
+
+var renderKeys = (json, keysName) => {
+    if (typeof json != "object") return;
+    if (keysName == undefined || keysName == "") {
+        keyFilters.length = 0;
+        counter = 0;
+        keysName = "";
+    }
+    if (json.length == undefined) {
+        var ks = Object.keys(json);
+        ks.filter(key => {
+            return keyFilters.filter((x) => `${keysName}-${key}` == x.id).length == 0;
+        }).forEach(key => {
+            const item = {
+                name : key,
+                id : `${keysName}-${key}`,
+                toString: function(){
+                    return `<a id="${this.id}" class="dropdown-item" href="#">${this.id}</a>`
+                }
+            };
+    
+            keyFilters.push(item);
+            renderKeys(json[key], item.id); 
+        });
+    } else {
+        json.forEach(key => {
+            renderKeys(key, keysName); 
+        });
+    }
+    
+    
+}
+
+var renderKeysOption = () => {
+    $('#keys-options').html(keyFilters.map(k => k.toString()).join(""));
+    keyFilters.forEach(function(k) {
+        console.log(`#${k.id}`)
+        $(`#${k.id}`).click(() => {
+            console.log(this);
+        })
+    })
+}
---
+++ template.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "widget" : 
+    { 
+  
+       "widget-title" : "Navigation",
+       "widget-id" : 1452345,
+       "widget-type" : "nav",
+       "base-url" : "http://joshkill.com",
+       "items" : [
+        {
+          "title" : "Home",
+          "description" : "Home is where the heart is",
+          "icon" : "btn-home.png",
+          "url" : "home.html"
+        },
+        {
+          "title" : "Services",
+          "description" : "We do it all, then some",
+          "icon" : "btn-services.png",
+          "url" : "services.html",
+          "Test": "test"
+        },
+        {
+          "title" : "Contact",
+          "description" : "Let's work together!",
+          "icon" : "btn-contact.png",
+          "url" : "contact.html"
+        }
+      ]
+    }
+  };
